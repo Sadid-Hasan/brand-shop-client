e6b5bd9204a61a3a3a08a287553565c29d687529
Sadid-Hasan <sadid.hasan94@gmail.com>
2023-10-22 12:52:44 +0600

    Mewsletter Section Done

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
 
 const Newsletter = () => {
     return (
         <div>
-            <h1>This is news letter</h1>
+           <section class="bg-slate-100 dark:bg-gray-900 font-poppins">
+    <div class="container px-4 py-16 mx-auto lg:flex lg:items-center lg:justify-between">
+        <h2 class="text-2xl font-poppins font-semibold tracking-tight text-gray-800 xl:text-3xl dark:text-white">
+            Join <span className=' font-cursive font-bold text-orange-500'>Car Connect X</span> for update & offers !
+        </h2>
+
+        <div class="mt-8 lg:mt-0">
+            <div class="flex flex-col space-y-3 sm:space-y-0 sm:flex-row sm:-mx-2">
+                <input id="email" type="text" class="px-4 py-2 text-gray-700 bg-white border border-gray-200 rounded-lg sm:mx-2 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-orange-400 dark:focus:border-orange-400 focus:ring-orange-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Email Address"/>
+
+                <button class="btn bg-orange-500  hover:bg-orange-400 focus:outline-none  text-white">
+                    Get Started
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from '../components/Banner';
 import Brand from '../components/Brand';
 import Feature from '../components/Feature';
+import Newsletter from '../components/Newsletter';
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Banner></Banner>
       <Brand></Brand>
       <Feature></Feature>
+      <Newsletter></Newsletter>
     </div>
   );
 };
