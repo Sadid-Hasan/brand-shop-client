62c6a0c9a1debd13987d4a89c4faa95068252e86
Sadid-Hasan <sadid.hasan94@gmail.com>
2023-10-23 10:46:23 +0600

    Button Destination Updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Banner = () => {
     return (
         <div className="w-full bg-center bg-cover h-[38rem]" style={{backgroundImage: 'url(https://i.ibb.co/KxtrQRX/pexels-torsten-dettlaff-70912.jpg)'}}>
@@ -5,7 +7,7 @@ const Banner = () => {
             <div className="text-center">
                 <h1 className="text-3xl text-white lg:text-6xl font-poppins font-bold">Discover Your Perfect Ride with  <span className=" text-orange-500">Car Connect X</span></h1>
                 <button className="btn bg-orange-500  hover:bg-orange-400 focus:outline-none  text-white">
-                    Get Quote</button>
+                <Link to='/signup'>Get Quote</Link></button>
 
                 <p className="text-xl text-white lg:text-2xl font-poppins font-semibold">
                 Where Driving Dreams Come True. Explore Quality, Style, and Performance Today!
