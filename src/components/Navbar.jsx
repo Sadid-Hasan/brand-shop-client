30137fc2e6894544c57c4c1a4f81ce98045ced26
Sadid-Hasan <sadid.hasan94@gmail.com>
2023-10-23 14:47:23 +0600

    Navbar Updated -User Icon added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,50 @@
-import React from 'react';
+
 import { NavLink } from 'react-router-dom';
+import userDefaultPic from '../assets/user.png';
 
 const Navbar = () => {
-  const menus =<>
-  <li><NavLink to ="/">Home</NavLink></li>
-  <li><NavLink to ="/add-product">Add Product</NavLink></li>
-  <li><NavLink to ="/my-cart">My Cart</NavLink></li>
-  </>
+  const menus = (
+    <>
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="/add-product">Add Product</NavLink></li>
+      <li><NavLink to="/my-cart">My Cart</NavLink></li>
+      <li><NavLink to="/login">Login</NavLink></li>
+      <li><NavLink to="/signup">Sign Up</NavLink></li>
+    </>
+  );
+
   return (
     <div className="navbar font-poppins bg-slate-50">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        {menus}
-      </ul>
-    </div>
-    <h1 className=" font-cursive text-orange-500 normal-case text-2xl">Car Connect X</h1>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-    {menus}
-    </ul>
-  </div>
-  <div className="navbar-end">
-  <ul className="menu menu-horizontal px-1">
-  <li><NavLink to ="/login">Login</NavLink></li>
- <li><NavLink to ="/signup">Sign Up</NavLink></li>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            {menus}
           </ul>
-  </div>
-</div>
-    );
+        </div>
+        <h1 className="font-cursive text-orange-500 normal-case text-2xl">Car Connect X</h1>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {menus}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <ul className="menu menu-horizontal px-1">
+          
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img src= {userDefaultPic} />
+        </div>
+      </label>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrPZCD63dkJeBLrHTVEGUUPneABgrvhX0",
+  authDomain: "brand-shop-df99a.firebaseapp.com",
+  projectId: "brand-shop-df99a",
+  storageBucket: "brand-shop-df99a.appspot.com",
+  messagingSenderId: "116655193970",
+  appId: "1:116655193970:web:204c6bcaf4914e8369a7eb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
